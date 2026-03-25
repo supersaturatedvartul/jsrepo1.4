24027e73e11b1760c9765f1ae9ba0ee979b7675f
supersaturatedvartul <bhujbalraoomkars@gmail.com>
2026-03-25 11:33:25 +0530

    '10th project'

---
+++ project10/index.js
@@ -0,0 +1,26 @@
+const dice = document.getElementById('dice');
+const roller = document.getElementById('roll');
+
+let value;
+
+roller.addEventListener('click',function (){
+    value = Math.floor(Math.random()*6)+1
+    if (value === 1) {
+        dice.innerHTML = `<img src="images/one.png" width=200px>`;
+    }
+    else if (value === 2) {
+        dice.innerHTML = `<img src="images/two.png" width=200px>`;
+    }
+    else if (value === 3) {
+        dice.innerHTML = `<img src="images/three.png" width=200px>`;
+    }
+    else if (value === 4) {
+        dice.innerHTML = `<img src="images/four.png" width=200px>`;
+    }
+    else if (value === 5) {
+        dice.innerHTML = `<img src="images/five.png" width=200px>`;
+    }
+    else  {
+        dice.innerHTML = `<img src="images/six.png" width=200px>`;
+    }
+});
