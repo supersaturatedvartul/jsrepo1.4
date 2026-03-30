5335cb26d30ab918e70f0e76c025f50c401651ed
supersaturatedvartul <bhujbalraoomkars@gmail.com>
2026-03-30 14:05:52 +0530

    'completion'

--- project15/index.js
+++ project15/index.js
@@ -12,12 +12,14 @@ submitbtn.addEventListener('click',function () {
         let task = input.value;
         tasklist.push(task);
         const li = document.createElement('li');
+        const spancont = document.createElement('span');
         const deletebtn = document.createElement('button');
         const check = document.createElement('input');
         check.type = 'checkbox';
         deletebtn.textContent = 'Delete';
-            li.textContent = task;
+            spancont.textContent = task;
             items.appendChild(li);
+            li.appendChild(spancont);
             li.appendChild(deletebtn);
             li.appendChild(check);
         deletebtn.addEventListener('click',function () {
@@ -30,10 +32,10 @@ submitbtn.addEventListener('click',function () {
 
         check.addEventListener('change',function () {
             if (check.checked) {
-                li.classList.add('striker');
+                spancont.classList.add('striker');
             }
             else {
-                li.classList.remove('striker');
+                spancont.classList.remove('striker');
             }
         })
     }
