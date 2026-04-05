85c8f6ca35f0afd64a7a13eae64c959deac7b625
supersaturatedvartul <bhujbalraoomkars@gmail.com>
2026-04-05 16:55:13 +0530

    '21th project done'

---
+++ project21/index.js
@@ -0,0 +1,81 @@
+const counter = document.getElementById('counter');
+const startbtn = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const resetbtn = document.getElementById('reset');
+const inc = document.getElementById('increase');
+const dec = document.getElementById('decrease');
+
+let count = 0;
+counter.textContent = count;
+if(count<=0) {
+    dec.disabled = true;
+}
+
+
+inc.addEventListener('click',function() {
+    count++;
+    counter.textContent = count;
+    checker(count);
+})
+
+dec.addEventListener('click',function() {
+    count--;
+    counter.textContent = count;
+    checker(count);
+})
+
+function checker (count) {
+    if(count > 0){
+    dec.disabled = false;
+}
+    else {
+    dec.disabled = true;
+}
+}
+
+let interval;
+
+function countdown() {
+    count--;
+    counter.textContent = count;
+    if (count <= 0) {
+        clearInterval(interval);
+        interval = null;
+    }
+}
+
+function toggledisable(status) {
+    inc.disabled = status;
+    dec.disabled = status;
+}
+
+
+startbtn.addEventListener('click',function () {
+    if(!interval && count>0) {
+    interval = setInterval(countdown,1000);
+    console.log(count);
+    toggledisable(true);
+    }
+})
+
+stopbtn.addEventListener('click',function () {
+    clearInterval(interval);
+    interval = null;
+    if (count >0) {
+        toggledisable(false);
+    }
+    else if (count === 0 ) {
+        dec.disabled = true;
+    }
+});
+
+resetbtn.addEventListener('click',function() {
+    count = 0;
+    counter.textContent = count;
+    clearInterval(interval);
+    interval = null;
+    if (count === 0) {
+        dec.disabled = true;
+        inc.disabled = false;
+    }
+})
